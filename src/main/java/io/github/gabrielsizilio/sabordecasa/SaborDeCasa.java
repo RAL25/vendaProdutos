39408fbbd95e08f8c0627707a6871cc0ad199939
yodemisj <yodemisj@gmail.com>
2022-12-03 01:48:17 -0300

    código de teste do Dao na main

--- src/main/java/io/github/gabrielsizilio/sabordecasa/SaborDeCasa.java
+++ src/main/java/io/github/gabrielsizilio/sabordecasa/SaborDeCasa.java
@@ -5,12 +5,15 @@
 
 package io.github.gabrielsizilio.sabordecasa;
 
+import io.github.gabrielsizilio.sabordecasa.database.FuncionarioDao;
 import io.github.rianal25.sabordecasa.cliente.Cliente;
 import io.github.rianal25.sabordecasa.cliente.Endereco;
 import io.github.rianal25.sabordecasa.cliente.Telefone;
 import io.github.gabrielsizilio.sabordecasa.produto.Item;
 import io.github.gabrielsizilio.sabordecasa.produto.Produto;
 import io.github.gabrielsizilio.sabordecasa.produto.Recheio;
+import io.github.yodemisj.sabordecasa.funcionario.Credencial;
+import io.github.yodemisj.sabordecasa.funcionario.Funcionario;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +25,14 @@ import java.util.List;
 public class SaborDeCasa {
 
     public static void main(String[] args) throws Exception {
+        Credencial cred = new Credencial("Yodemis@gmail.com","12345");
+        Funcionario func = new Funcionario(1l,"Yodemis",cred,true,true);
+        Long id;
         
+        System.out.println(">> " + func);
+        
+        id = new FuncionarioDao().saveOrUpdate(func);
+        func.setId(id);
         /*Recheio recheio = new Recheio(1L, "Recheio de carne", "Carne com molho", new BigDecimal("2"));
         Produto produto = new Produto(2L, "Mineirinho", new BigDecimal("10"), recheio);
         Item item = new Item(produto, 3);
@@ -38,7 +48,7 @@ public class SaborDeCasa {
 //        
 //        System.out.println(">> "+ e);
 
-        Endereco endereco1 = new Endereco("18", (short)80, "Village", "casa");
+/*        Endereco endereco1 = new Endereco("18", (short)80, "Village", "casa");
         Telefone telefone1 = new Telefone((byte)38, 12341234, false);
         
         Endereco endereco2 = new Endereco("17", (short)81, "Minas Gerais", "apt 101");
@@ -51,6 +61,6 @@ public class SaborDeCasa {
         cliente1.addEndereco(endereco2);
         
         System.out.println(">>" + cliente1);
-        
+*/        
     }
 }
